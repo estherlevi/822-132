504a73da6061178b34dd06c5791409c91579e95b
jbt <jbt@192.168.0.71>
2021-05-24 21:21:38 +0300

    lesson 1

---
+++ lesson 01/src/c/App3.java
@@ -0,0 +1,14 @@
+package c;
+
+public class App3 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		String name = "David";
+		
+		System.out.println(a);
+		System.out.println(name);
+
+	}
+
+}

8223907dc60a250d1cac590550d30e00d8beeb47
jbt <jbt@192.168.0.71>
2021-05-27 19:40:57 +0300

    lesson 2

---
+++ lesson 02 identifiers keywords and types/src/a/Demo4Targil.java
@@ -0,0 +1,42 @@
+package a;
+
+public class Demo4Targil {
+
+	public static void main(String[] args) {
+		// integers
+		byte n1 = 5;
+		short n2 = 10;
+		int n3 = 15;
+		long n4 = 20;
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		// decimals
+		float f1 = 5.3F;
+		double d1 = 6.32;
+		
+		// logical
+		boolean b1 = true;
+		boolean b2 = false;
+		boolean b3 = n1 < n2;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		
+		// characters - used for text
+		char c1 = 'A';
+		char c2 = '÷';
+		char c3 = '1';
+		char c4 = '.';
+		char c5 = ' ';
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		
+		
+	}
+
+}

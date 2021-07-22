e1f924bd8294e37100f63ebef3010a49e9d44f9e
jbt <jbt@192.168.0.53>
2021-07-22 18:22:56 +0300

    aaa

---
+++ lesson 15 collections sort/src/a/Demo1.java
@@ -0,0 +1,54 @@
+package a;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		{ // numerals natural order is from lower to higher
+			Set<Integer> set = new TreeSet<>();
+			set.add(4);
+			set.add(7);
+			set.add(1);
+			set.add(-8);
+			set.add(3);
+			System.out.println(set);
+		}
+		{ // Date natural order is from early to late
+			Set<LocalDate> set = new TreeSet<>();
+			set.add(LocalDate.of(2021, 10, 22));
+			set.add(LocalDate.of(2021, 7, 22));
+			set.add(LocalDate.of(2021, 3, 22));
+			System.out.println(set);
+		}
+
+		{ // String natural order is alphabetically
+			Set<String> set = new TreeSet<>();
+			set.add("xml");
+			set.add("java");
+			set.add("html");
+			set.add("apple");
+			System.out.println(set);
+		}
+
+		{ // Person natural order is Person.id
+			Set<Person> set = new TreeSet<>();
+			set.add(new Person(102, "Dal", 30));
+			set.add(new Person(101, "Tova", 25));
+			set.add(new Person(103, "Amir", 5));
+			System.out.println(set);
+		}
+		{ // Person other order: Person.name
+			Set<Person> set = new TreeSet<>();
+			set.add(new Person(102, "Dal", 30));
+			set.add(new Person(101, "Tova", 25));
+			set.add(new Person(103, "Amir", 5));
+			System.out.println(set);
+		}
+
+	}
+
+}
